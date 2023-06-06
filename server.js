67376358db295ea86fb2b6c0eadaa8891fabe31a
erubae <rombuski@gmail.com>
2023-06-06 22:42:10 +0100

    done cors changes

--- server.js
+++ server.js
@@ -9,21 +9,34 @@ const cookieparser = require('cookie-parser');
 const path = require('path')
 app.use(cookieparser())
 app.use(express.json())
+const corsOptions = {
+    origin: true,
+    credentials: true,
+}
+app.use(cors(corsOptions))
 
-app.use(cors({
-    origin: "https://rom-net.onrender.com",             //['http://localhost:3000', 'https://rom-net.onrender.com'],
-   // credentials: true
-}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    );
+    if ("OPTIONS" == req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  });
 
 app.use(routes)
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    });
-   }
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
