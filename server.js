53676c9ff72089a48d2f9061ec2d3bd60a6ebf19
erubae <rombuski@gmail.com>
2023-06-06 18:00:00 +0100

    changed

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(cors({
 }))
 app.use(routes)
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+   }
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
